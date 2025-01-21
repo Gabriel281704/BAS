af807b7ca1b170bfc7d96680ca9134534a6e8b12
Gabriel281704 <g281704@dac.unicamp.br>
2025-01-21 17:51:57 -0300

    Update Blink.py

--- Blink.py
+++ Blink.py
@@ -9,15 +9,9 @@ import utime
 ###########################################################################################
 
 # O pino 25 é usado no led interno da placa...
-led = machine.Pin(0, machine.Pin.OUT)
-led1 = machine.Pin(25, machine.Pin.OUT)
+led = machine.Pin(25, machine.Pin.OUT)
 
 # Agora basta ligar e desligar o led com o passar do tempo
-
-led1.value(1)
-utime.sleep(0.4)
-led1.value(0)
-utime.sleep(0.4)
 
 while(True): # Garante uma repetição infinita do processo
     led.value(0)
